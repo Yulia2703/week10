6af03afc60197bb5629f77c8c68c53e614233e6a
Yulia2703 <giuli.k@libero.it>
2025-01-20 11:21:08 +0100

    jshomework

---
+++ assets/script/index.js
@@ -0,0 +1,10 @@
+//console.log(`Я учу JavaScript!`)
+
+//function sayHello () {
+//    console.log(`Я учу JavaScript!`);
+//}
+//sayHello();
+
+let sayHello =() => console.log ("Я учу JavaScript!");
+sayHello();
+
