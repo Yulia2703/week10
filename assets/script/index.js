56f043ba1f1917a86a4738a56c1bba202e568099
Yulia2703 <giuli.k@libero.it>
2025-01-22 07:28:18 +0100

    2201

--- assets/script/index.js
+++ assets/script/index.js
@@ -8,3 +8,11 @@
 let sayHello =() => console.log ("Я учу JavaScript!");
 sayHello();
 
+const image = document.getElementById('elem-img');
+console.log(image);
+function showImgOne() { 
+    image.src="./assets/style/img/cat2.jpg";   
+}
+function showImgTwo() { 
+    image.src="./assets/style/img/cat1.jpg";   
+}
